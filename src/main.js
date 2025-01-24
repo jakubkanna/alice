de3b3e353acc2280f44dd6b73b83c407a467dc66
jakubkanna <hello.jakubkanna@gmail.com>
2025-01-25 00:01:42 +0100

    deploy with gh-pages

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "@google/model-viewer";
 import "@google/model-viewer-effects";
 import view from "./script.js";
-
+import "./styles.css";
 // Initialize after model-viewer and effects are loaded
 window.addEventListener("DOMContentLoaded", () => {
   const modelViewer = document.getElementById("alice");
